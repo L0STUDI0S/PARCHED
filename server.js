7a9c94be7fc5a0669497560eecc831d76e72bcb2
L0STUDI0S <wafflesulloa@gmail.com>
2025-12-04 20:39:10 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import express from "express";
 import fs from "fs";
 import webpush from "web-push";
-import path from "path";
 
 const app = express();
 app.use(express.json());
 
-// Serve static files from the root of the repo
-app.use(express.static("PARCHED"));
+// Serve static files from the root
+app.use(express.static("."));
 
 // VAPID keys
 const publicVapidKey = "BAgmoSmBrF591eDzhJ-54OZYJBU3gYt9j40ZxqQzTOURlcBiBmUqZHkVD5ja_0Wmp0dan0Q2wrphEO1pYHkzTmI";
 const privateVapidKey = "whKH9NrcmO89SS2L-s8i9CeyfB46Kps_vo-tHAE29sk";
+
 webpush.setVapidDetails("mailto:you@example.com", publicVapidKey, privateVapidKey);
 
-// subscribers.json path
-const subscribersPath = path.join("PARCHED", "subscribers.json");
+// subscribers.json
+const subscribersPath = "./subscribers.json";
 let subscribers = [];
 if (fs.existsSync(subscribersPath)) {
     subscribers = JSON.parse(fs.readFileSync(subscribersPath, "utf8"));
@@ -35,7 +35,7 @@ app.post("/notify", async (req, res) => {
     const payload = JSON.stringify({
         title: req.body.title,
         body: req.body.body,
-        icon: req.body.icon,
+        icon: req.body.icon
     });
 
     for (let i = subscribers.length - 1; i >= 0; i--) {
